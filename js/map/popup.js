18fb881687e7e407b1b422843ef304d362c0cedf
mmolta <mcmolta10@gmail.com>
2023-04-27 19:31:35 -0400

    garden hover popup

--- js/map/popup.js
+++ js/map/popup.js
@@ -13,10 +13,19 @@ const makeThumbClickPopup = () => {
 }
 
 const makeThumbHoverPopup = (props) => {
-    
+    const span = document.createElement('span')
+    const strong = document.createElement('strong')
+
+    span.classList.add('popup-span')
+
+    strong.textContent = props.gardenname
+
+    span.appendChild(strong)
+
+    return span
 }
 
-const makeParkPopup = () {
+const makeParkPopup = () => {
 
 }
 
